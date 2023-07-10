139de91620f85471a9120839e8ece923ebded521
Ksivol <surkin-ant@mail.com>
2023-07-10 23:10:44 +0400

    Создан класс NewNoteActivity

---
+++ app/src/main/java/com/ksivol_project/shoppinglist/activities/NewNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.ksivol_project.shoppinglist.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ksivol_project.shoppinglist.R
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
