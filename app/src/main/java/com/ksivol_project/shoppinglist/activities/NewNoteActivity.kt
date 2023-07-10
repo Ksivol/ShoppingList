1d119060e00d73027585826e966700711b3b36a1
Ksivol <surkin-ant@mail.com>
2023-07-10 23:40:24 +0400

    Добавлены кнопки и view для создания заметок

--- app/src/main/java/com/ksivol_project/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/ksivol_project/shoppinglist/activities/NewNoteActivity.kt
@@ -2,11 +2,38 @@ package com.ksivol_project.shoppinglist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.ksivol_project.shoppinglist.R
+import com.ksivol_project.shoppinglist.databinding.ActivityNewNoteBinding
 
 class NewNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewNoteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_note)
+        binding = ActivityNewNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        actionBarSettings()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.new_note_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.id_save){
+
+        } else if (item.itemId == android.R.id.home){
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun actionBarSettings(){
+        val ab = supportActionBar
+        ab?.setDisplayHomeAsUpEnabled(true)
+    }
+
 }
